5080a56455b1b18e8183ef0a154fba18a6ee3fd7
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2021-11-05 19:38:14 -0400

    Edited Generator

--- day-1 bandname-generator/bandname-generator.py
+++ day-1 bandname-generator/bandname-generator.py
@@ -2,10 +2,10 @@
 print("Welcome to the Band Name Generator.")
 
 # Asking user for city they grew up in
-city = input("What's the name of the city you grew up in?\n")
+city = input("What's the name of the city you grew up in?\n").strip()
 
 # Asking user for name of pet
-pet = input("What is your pet's name?\n")
+pet = input("What is your pet's name?\n").strip()
 
 # Printing out band name
 print("Your band name could be " + city + ' ' + pet +'.')
