567784372349bab9a6046294342ca313882dc408
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2021-12-20 11:56:51 -0500

    Turtle Spirograph

---
+++ day18_turtle-graphics/day18_turtle-spirograph.py
@@ -0,0 +1,21 @@
+import turtle
+import random
+
+tim = turtle.Turtle()
+turtle.colormode(255)
+tim.speed(0)
+
+def randomColor():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b)
+
+for i in range(10,360,10):
+    tim.color(randomColor())
+    tim.circle(100)
+    current_heading = tim.heading()
+    tim.setheading(current_heading + 10)
+
+screen = turtle.Screen()
+screen.exitonclick()
