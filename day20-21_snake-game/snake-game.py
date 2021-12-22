77e8f9ed377eac4ebadd2529fafd0985c96f4d25
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2021-12-22 16:03:00 -0500

    Snake game

--- day20-21_snake-game/snake-game.py
+++ day20-21_snake-game/snake-game.py
@@ -1,28 +1,43 @@
 import turtle 
-import random
+import time
 
-def move_forward():
-    tim.forward(10)
+STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
 
-def move_left():
-    tim.left(90)
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+    
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = turtle.Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.snake.append(new_segment)
+    
+    def move(self):
+        for seg in range(len(self.snake) -1, 0, -1):
+                new_x = self.snake[seg- 1].xcor()
+                new_y = self.snake[seg- 1].ycor()
+                self.snake[seg].goto(new_x, new_y)
+        self.snake[0].forward(MOVE_DISTANCE)
 
-def move_right():
-    tim.right(90)
-
-
-tim = turtle.Turtle(shape="square")
-tim.turtlesize(stretch_len=3, stretch_wid=0.5)
-tim.penup()
+# Main
 screen = turtle.Screen()
-
+screen.setup(width=600, height=600)
+screen.title("Snake Game")
 screen.bgcolor("black")
-tim.color("white")
+screen.tracer(0)
 
-screen.listen()
-screen.onkeypress(fun = move_forward, key = "Up")
-screen.onkeypress(fun = move_left, key = "Left")
-screen.onkeypress(fun = move_right, key = "Right")
-screen.onkeyrelease(fun = move_forward, key = "Up")
+snake = Snake()
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    
 screen.exitonclick()
