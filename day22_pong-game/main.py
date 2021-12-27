64847d1fd7aec8bd0602538efe271cd2148f7bb8
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2021-12-27 12:12:41 -0500

    Pong Game

--- day22_pong-game/main.py
+++ day22_pong-game/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+import time 
 
 # Create Screen
 screen = Screen()
@@ -9,20 +11,42 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-# Create Paddles
+# Create Paddles and Ball
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
+scoreboard = Scoreboard()
 
+# Moving the paddles
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    # Detect when left paddle misses
+    if ball.xcor() < - 380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
