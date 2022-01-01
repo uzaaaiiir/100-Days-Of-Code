b3ce0f8e335db0d8ffd8ee1ef97b59bed88a396b
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-01-01 11:07:09 -0500

    US States Game

--- day25_CSV-files/main.py
+++ day25_CSV-files/main.py
@@ -1,15 +1,12 @@
 import turtle 
 import pandas
 
-FONT = ("Courier", 5, "bold")
-
-def write_to_screen(x,y, state):
-    writer.penup()
-    writer.goto(x,y)
-    writer.write(state, align="left", font=FONT)
-
+# Create a screen writer turtle
 writer = turtle.Turtle()
 writer.hideturtle()
+writer.penup()
+
+# Creating screen with gif image 
 tim = turtle.Turtle()
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -17,26 +14,29 @@ screen.title("US States Game")
 screen.addshape("blank_states_img.gif")
 tim.shape("blank_states_img.gif")
 
-
+# Reading State Data
 data = pandas.read_csv("50_states.csv")
 states = data["state"].to_list()
-print(states)
 
 count = 0
 total = 50
 
-while count <= 50:
-    user_input = screen.textinput(title=f"States {count}/{total}", prompt="Enter a State:")
+while count < 50:
+    user_input = screen.textinput(title=f"States {count}/{total}", prompt="Enter a State:").title()
     
-    if user_input.title() in states:
-        states.remove(user_input.title())
-        state = data[data.state==user_input.title()]
-        print(state)
-        x = state.x
-        y = state.y
-        write_to_screen(int(x), int(y), user_input.title())
+    if user_input == "Exit":
+        # States to learn .csv 
+        states = pandas.DataFrame(states)
+        states.to_csv("US_states.csv")
+        break
+
+    if user_input in states:
+        states.remove(user_input)
+        state = data[data.state==user_input]
+        writer.goto(int(state.x), int(state.y))
+        writer.write(user_input)
         count+=1
-    
 
 
-turtle.mainloop()
+
+screen.exitonclick()
