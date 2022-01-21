22862175ffe4483520215acba0d52acb0efb47d9
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-01-20 21:30:38 -0500

    Password Generator GUI

--- day28_pomodoro-app/main.py
+++ day28_pomodoro-app/main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1.5
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -15,7 +15,7 @@ timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
-    window.after_cancer(timer)
+    window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
     title_label.config(text="Timer")
     check_marks.config(text="")
